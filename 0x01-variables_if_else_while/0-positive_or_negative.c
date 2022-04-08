669d8ecb6216c484b8bd22da8fe6843aca59c456
Nnamdi Anyanwu <nnamdi.prime@gmail.com>
2022-04-08 16:48:03 -0700

    Update task 0 again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -11,17 +11,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive");
+printf("is positive\n");
 }
 else
 {
 if (n < 0)
 {
-printf("is negative");
+printf("is negative\n");
 }
 else
 {
-printf("is zero");
+printf("is zero\n");
 }
 }
 return (0);
