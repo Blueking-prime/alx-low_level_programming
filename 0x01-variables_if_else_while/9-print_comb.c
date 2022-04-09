f20df536030e2753a24c2d151bcd68c29f1e6d81
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-09 02:36:28 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,13 @@
 int main(void)
 {
 int c;
-int d;
 
 for (c = 0; c <= 9; c++)
 {
-for (d = 0; d <= 9; d++)
-{
 putchar (c + '0');
-putchar (d + '0');
 putchar (',');
 putchar(' ');
 }
-}
+
 return (0);
 }
