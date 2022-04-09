df74c4847de76af7d0142d55b814c5a142b36a9e
Nnamdi Anyanwu <nnamdi.prime@gmail.com>
2022-04-08 17:36:49 -0700

    Update placeholder tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,12 @@
 */
 int main(void)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+/**
+*int n;
+*
+*srand(time(0));
+*n = rand() - RAND_MAX / 2;
+*/
 /* your code goes there */
 return (0);
 }
