f80e1fef02b9bf897722f4ec08df73ccc5a26a0e
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-09 02:02:23 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Return: Always (Success/correct)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != q)
+{
+if (c != e)
+{
+putchar (c);
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
