d238eeae0447cfcd3da1762c109994f7574ddc5e
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-09 01:56:10 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: Always (Success/correct)
 */
 int main(void)
 {
-/**
-*int n;
-*
-*srand(time(0));
-*n = rand() - RAND_MAX / 2;
-*/
-/* your code goes there */
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar (c);
+}
+
+putchar('\n');
 return (0);
 }
