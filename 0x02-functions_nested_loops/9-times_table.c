eac3e83c95a2097b09ca702d6a2f862bcdadcce6
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 22:24:53 +0100

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* times_table - Entry point
+*
+* Return: (Always/Success)
+*/
+
+void times_table(void)
+{
+int a = 0;
+int b = 0;
+int res;
+while (a <= 9)
+{
+while (b <= 9)
+{
+res = a * b;
+if (res < 10)
+{
+_putchar (' ');
+_putchar (' ');
+}
+else
+{
+_putchar (' ');
+}
+_putchar(res + '0')
+b++;
+if (b = 9)
+{
+_putchar ('\n')
+}
+}
+a++;
+}
+}
