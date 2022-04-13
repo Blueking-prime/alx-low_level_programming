c68e63ec7ebfbfec2c3648088b3ccd1e0fde9f9c
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 22:41:49 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,32 +7,48 @@
 */
 
 void times_table(void)
+#include "holberton.h"
+
+/**
+ * times_table - prints times table
+ * Return:void
+ */
+
+void times_table(void)
 {
+
 int a = 0;
-int b = 0;
+int b;
 int res;
 while (a <= 9)
 {
+b = 0;
 while (b <= 9)
 {
-res = a * b;
-if (res < 10)
+rep = a * b;
+if (b == 0)
 {
-_putchar (' ');
-_putchar (' ');
+_putchar(res + '0');
+}
+else if (res < 10)
+{
+_putchar(' ');
+_putchar(res + '0');
 }
 else
 {
-_putchar (' ');
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
 }
-_putchar(res + '0');
+
+if (b < 9)
+{
 _putchar(',');
+_putchar(' ');
+}
 b++;
 }
-if (b == 9)
-{
 _putchar('\n');
-}
 a++;
 }
 }
