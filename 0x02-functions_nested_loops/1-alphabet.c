18aa223dd6b4a6ffbfb6100859ed8079849f8151
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 18:57:42 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,11 @@
 * Return: 0
 */
 
-void print_alphabet(void){
+void print_alphabet(void)
+{
 char a;
-for (a = 'a'; a <= 'z'; a++){
+for (a = 'a'; a <= 'z'; a++)
+{
 _putchar(a);
 }
 _putchar('\n');
