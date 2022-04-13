ef241c430e559ae83fcca0ea576c9ce430dc4bd9
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 19:23:40 +0100

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _islower - Entry point
+*
+* Return: Always
+*/
+
+int _islower(int c)
+{
+if (c < 122)
+{
+if (c > 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+else
+{
+return (0);
+}
+}
+}
