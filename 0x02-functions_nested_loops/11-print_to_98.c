527ac2a0c80d0f0801f8d8e40e8a2217b29d06d6
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 23:00:03 +0100

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - Entry point
+*
+* @n: number to be operated on
+*
+* Return: (Always/Success)
+*/
+
+void print_to_98(int n)
+int a;
+{
+if (n < 98)
+{
+for (a = n; a <= 98; a++)
+{
+printf ("%d, ", a);
+}
+}
+else if (n > 98)
+{
+for (a = n; a >= 98; a--)
+{
+printf ("%d, ", a);
+}
+}
+else
+{
+printf ("98")
+}
+}
