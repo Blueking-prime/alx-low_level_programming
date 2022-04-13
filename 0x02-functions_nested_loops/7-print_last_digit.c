5e9dcb10aec4d86ddaeedbbcc1d07e223711a9fa
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 20:21:05 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_last_digit - Entry point
+* @n: integer to be calculated
+* Return: (Always/Success)
+*/
+
+
+int print_last_digit(int n)
+{
+int res;
+
+if (n < 0)
+{
+res = -1 * (n % 10);
+}
+else
+{
+res = n % 10;
+}
+return (res);
+}
