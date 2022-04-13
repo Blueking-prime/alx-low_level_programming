776ab07823d74fd6b1b2f09babb7c2c5efb299c6
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 19:06:10 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+* print_alphabet_x10 - Entry point
+*
+* Return: Always
+*/
+
+void print_alphabet_x10(void)
+{
+char a;
+int b;
+while (b = 0; b < 10; b++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n')
+}
+return;
+}
