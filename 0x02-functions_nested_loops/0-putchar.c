de4d6f1918708d39e3dca1cf56d6343c89d2ed28
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 23:13:12 +0100

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: (Always/Success)
+*/
+
+int main(void)
+{
+char c[9] = "_putchar";
+int a;
+for (a = 0; a < 8; a++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
