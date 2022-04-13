e3ca39d08c1bdd49b785f5f2b0d37bbd40a298e6
Blueking-prime <72868944+Blueking-prime@users.noreply.github.com>
2022-04-13 19:37:42 +0100

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _isalpha - Entry point
+* @c: character who's case is to be tested
+* Return: 0 or 1
+*/
+
+int _isalpha(int c)
+{
+if (c <= 122 && c >= 97)
+{
+return (1);
+}
+else if (c <= 90 && >= 65)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
