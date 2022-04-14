d3d407ca41ff76b70b1e60803f0f8d672960b02e
Nnamdi Anyanwu <blueking189@gmail.com>
2022-04-14 23:46:41 +0100

    Update 9-fizz_buzz.c
    even MORE testing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,27 +10,30 @@ int main(void)
 {
 int i;
 
-for (i = 1; i < 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (((i % 3) == 0) && ((i % 5) == 0))
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if ((i % 3) == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if ((i % 5) == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf("%d ", i);
-}
+printf("%d", i);
 }
 
-printf("100\n");
-
+if (i != 100)
+{
+putchar(' ');
+}
+}
+putchar('\n');
 return (0);
 }
