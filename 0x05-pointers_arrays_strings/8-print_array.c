17a46e057c5aacff87fd95e51ae757f8af96b927
Nnamdi Anyanwu <blueking189@gmail.com>
2022-04-21 00:18:36 +0100

    Revert task 8 and Update task 9
    Will study the mechanics of task 8 more before I submit. Meanwhile tried the other solution to task 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@
 void print_array(int *a, int n)
 {
 int i = 0;
-for (n--; n >= 0; n--, i++)
+for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
 if (n > 0)
@@ -22,6 +22,6 @@ if (n > 0)
 printf(", ");
 }
 }
-printf("\n");
+printf("%d\n", a[i]);
 return;
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,7 +12,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-dest = src;
+int i = 0;
+while (src[i] != '\0')
+{
+desc[i] = src[i];
+i++;
+}
+
 
 return (dest);
 }
