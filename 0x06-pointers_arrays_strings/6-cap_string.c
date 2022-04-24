4fe446cb3932f48129f5d81cb8948b5f8a94b855
Nnamdi Anyanwu <blueking189@gmail.com>
2022-04-25 00:59:57 +0100

    Add task 7 and modify task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,11 @@ char *cap_string(char *s)
 int i;
 for (i = 0; s[i] != '\0'; i++)
 {
+if (i == 0)
+{
+if (s[i] >= 97 && s[i] <= 122)
+s[i] -= 32;
+}
 if (s[i] == 9 || s[i] == ' ' || s[i] == '\n' || s[i] == ',' || s[i] == '(')
 {
 i++;
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,5 +10,20 @@
 
 char *leet(char *s)
 {
-    return (s);
+int i, j;
+char letters[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+char nums[5] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == letters[j])
+{
+s[i] = nums[j];
+}
+
+}
+}
+
+return (s);
 }
