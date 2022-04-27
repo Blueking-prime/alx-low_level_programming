8ed917d3fce50cd011ba0b6619a297b862671c61
Nnamdi Anyanwu <blueking189@gmail.com>
2022-04-27 02:42:42 +0100

    Update 2-strchr.c
    Removing redundant code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,23 +13,14 @@
 char *_strchr(char *s, char c)
 {
 int i;
-int j = 0;
 
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == c)
 {
-j++;
-break;
+return (s + i);
 }
 }
 
-if (j != 0)
-{
-return (s + i);
-}
-else
-{
 return ('\0');
-}
 }
