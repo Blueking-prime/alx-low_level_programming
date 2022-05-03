ce4c08c374d2d2499a744f94d3c73f45d6e52182
Nnamdi Anyanwu <blueking189@gmail.com>
2022-05-03 01:37:12 +0100

    Add newline

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,10 +12,10 @@
 
 int main(int argc, char *argv[])
 {
-printf("%s", argv[0]);
+printf("%s\n", argv[0]);
 if (argc < 0)
 {
-printf("%s", argv[0]);
+printf("%s\n", argv[0]);
 }
 return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,7 +12,7 @@
 
 int main(int argc, char **argv)
 {
-printf("%d", argc);
+printf("%d\n", argc);
 if (argv[0] == argv[1])
 {
 return (0);
