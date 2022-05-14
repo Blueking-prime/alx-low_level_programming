6ce4aad1cff1c62c9c70a4b225cf44cd0e27acab
Nnamdi Anyanwu <blueking189@gmail.com>
2022-05-14 01:16:02 +0100

    Minor tweaks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,9 +14,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (size != NULL && action != NULL)
-{
 size_t i;
+if (action != NULL)
+{
 for (i = 0; i < size; i++)
 {
 action(array[i]);
