87380b2a107c472a91f15606b8e9134273635dfe
Nnamdi Anyanwu <blueking189@gmail.com>
2022-05-14 01:41:58 +0100

    Update 3-main.c
    I don't know what I'm doing but more testing

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,7 +2,6 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
 * main - Entry point
@@ -27,19 +26,19 @@ printf("Error\n");
 exit(98);
 }
 
-if ((argv[2] == "/" || argv[2] == "%") && argv[3] == "0")
+if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 {
 printf("Error\n");
 exit(100);
 }
-
-c = get_op_func(argv[2])(a, b);
 
 if (get_op_func(argv[2]) == NULL)
 {
 printf("Error\n");
 exit(99);
 }
+
+c = get_op_func(argv[2])(a, b);
 
 printf("%d\n", c);
 
