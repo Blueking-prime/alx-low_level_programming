fdf62f29aee4db96d42e827dfd4f7677e203220e
Nnamdi Anyanwu <blueking189@gmail.com>
2022-05-14 01:33:54 +0100

    Update 2-int_index.c
    Testing

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (array != NULL || cmp != NULL)
+if (array == NULL || cmp == NULL)
 {
+return (-1);
+}
+
 if (size <= 0)
 {
 return (-1);
 }
+
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
@@ -29,6 +33,6 @@ if (cmp(array[i]))
 return (i);
 }
 }
-}
+
 return (-1);
 }
