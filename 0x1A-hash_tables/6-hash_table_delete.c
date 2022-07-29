3d4332933cd14fc1180137805dde4758401d2f90
Nnamdi Anyanwu <blueking189@gmail.com>
2022-07-29 23:28:49 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,34 +10,25 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int current_idx;
+	unsigned long int current_idx = 0;
 	hash_node_t *node;
 	hash_node_t *next;
 
 	if (ht == NULL) /* Checks if hash table exists */
 		return;
 
-	for (current_idx = 0; current_idx < ht->size; current_idx++)
+	while (current_idx < ht->size)
 	{
 		node = ht->array[current_idx];
-		if (node->next == NULL)
+		while (node != NULL)
 		{
+			next = node->next;
 			free(node->key);
 			free(node->value);
 			free(node);
+			node = next;
 		}
-		else
-		{
-			while (node != NULL)
-			{
-				next = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = next;
-			}
-			free(ht->array[current_idx]);
-		}
+		current_idx++;
 	}
 	free(ht->array);
 	free(ht);
