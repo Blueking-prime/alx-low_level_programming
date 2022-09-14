b9b37072dcc9b5170e280452b438f12dc1137241
Nnamdi Anyanwu <blueking189@gmail.com>
2022-09-15 00:53:00 +0100

    Add technical interview question

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''Island Perimeter Interview'''
+
+
+def island_perimeter(grid):
+    '''Calculates the permeter of a gridlike island'''
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                try:
+                    n = grid[i - 1][j]
+                except IndexError:
+                    n = 0
+                try:
+                    e = grid[i][j + 1]
+                except IndexError:
+                    e = 0
+                try:
+                    s = grid[i - 1][j]
+                except IndexError:
+                    s = 0
+                try:
+                    w = grid[i][j - 1]
+                except IndexError:
+                    w = 0
+                perimeter += check_surroundings(n, e, s, w)
+    return perimeter
+
+def check_surroundings(n, e, s, w):
+    '''Checks all tiles around a land tile'''
+    matches = 0
+    if n == 0:
+        matches += 1
+    if e == 0:
+        matches += 1
+    if s == 0:
+        matches += 1
+    if w == 0:
+        matches += 1
+    return matches
